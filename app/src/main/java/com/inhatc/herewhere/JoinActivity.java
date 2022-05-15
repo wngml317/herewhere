0dc901a004db47e80c5c4b2efa8fa7a99561650b
wngml317 <wngml317@naver.com>
2022-05-15 15:53:41 +0900

    20220515_Join_Activity

---
+++ app/src/main/java/com/inhatc/herewhere/JoinActivity.java
@@ -0,0 +1,15 @@
+package com.inhatc.herewhere;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class JoinActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_join);
+    }
+}
